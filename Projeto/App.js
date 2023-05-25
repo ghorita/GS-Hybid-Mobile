d8a0a016536ba851406d96cbfc66ef9536e854eb
ghorita <rm94860@fiap.com.br>
2023-05-25 11:00:44 -0300

    instalando todos os imports

--- Projeto/App.js
+++ Projeto/App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, TextInput, StyleSheet, Image, ImageBackground, ScrollView, FlatList} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import{createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+    <View></View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
